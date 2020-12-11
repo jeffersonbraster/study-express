04c2411ce53fc0aa3ef2f060290c6a47fc1ac95c
Jefferson <jeffersonbraster@gmail.com>
2020-12-10 23:22:58 -0300

    Aggregate - calculando o average course cost

--- models/Course.js
+++ models/Course.js
@@ -15,7 +15,7 @@ const CourseSchema = new mongoose.Schema({
     required: [true, "Adicione um numero para Semanas."],
   },
   tuition: {
-    type: String,
+    type: Number,
     required: [true, "valor da taxa obrigatorio."],
   },
   minimumSkill: {
@@ -36,6 +36,39 @@ const CourseSchema = new mongoose.Schema({
     ref: "Bootcamp",
     required: true,
   },
+});
+
+//Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Call getAveregeCost after save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Call getAveregeCost before save
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
 });
 
 module.exports = mongoose.model("Course", CourseSchema);
