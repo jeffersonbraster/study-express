42c24d2d936ccd415c6632bb30a24a4f79927289
Jefferson <jeffersonbraster@gmail.com>
2020-12-12 14:26:22 -0300

    auth protect middleware

---
+++ middleware/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+
+//rotas protegidas
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  //  else if(req.cookies.token) {
+  //   token = req.cookies.token
+  // }
+  //make sure token exist
+  if (!token) {
+    return next(
+      new ErrorResponse("Sem autorização para acessar sua solicitação.", 401)
+    );
+  }
+
+  try {
+    //verificar token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (error) {
+    new ErrorResponse("Sem autorização para acessar sua solicitação.", 401);
+  }
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+const { register, login, getMe } = require("../controllers/auth");
 
 const router = express.Router();
 
+const { protect } = require("../middleware/auth");
+
+router.get("/me", protect, getMe);
 router.post("/register", register);
 router.post("/login", login);
 
