996b2ba437f4c0ab9273af855feb7acb25373cee
Jefferson <jeffersonbraster@gmail.com>
2020-12-13 10:59:24 -0300

    forgot pass - gerando token

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,3 +82,24 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     data: user,
   });
 });
+
+//@desc   Forgot password
+//@route  POST /api/v1/auth/forgotpassword
+//@access Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse(`Não existe usuário com este E-mail.`, 404));
+  }
+
+  //get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
