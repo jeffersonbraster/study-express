1ab2a72a3df955681c5f62f450278e1d1b6b63ac
Jefferson <jeffersonbraster@gmail.com>
2020-12-15 18:29:14 -0300

    update and delete review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -66,3 +66,60 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+//@desc   Update new review
+//@route  PUT /api/v1/reviews/:id
+//@access Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review não encontrado: ${req.params.id}`, 404)
+    );
+  }
+
+  //make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(`Sem autorização para atualizar review`, 404)
+    );
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(201).json({
+    success: true,
+    data: review,
+  });
+});
+
+//@desc   Delete new review
+//@route  DELETE /api/v1/reviews/:id
+//@access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`Review não encontrado: ${req.params.id}`, 404)
+    );
+  }
+
+  //make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(`Sem autorização para atualizar review`, 404)
+    );
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
