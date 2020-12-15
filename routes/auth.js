fbf19b888c08b8457de7d73fb42768e545a21d1b
Jefferson <jeffersonbraster@gmail.com>
2020-12-14 22:11:09 -0300

    update user details and password

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -12,9 +14,12 @@ const router = express.Router();
 const { protect } = require("../middleware/auth");
 
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgotpassword", forgotPassword);
+
 router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
